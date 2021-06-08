0f34643fff531a5dabe27ba33dfae1a4dac06282
Owen Mowery <owenmowery99@gmail.com>
2021-06-08 11:44:38 -0400

    commit tree 92756d3cf9f960507d718f716b1a6731bd66cb29

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Your code here
+function mapToNegativize(arr) {
+    return arr.map(function(element) {
+        if (element < 0) {
+            return Math.abs(element);
+        } 
+        else if (element > 0) {
+            return -Math.abs(element);
+        }
+    });
+};
+
+function mapToNoChange(arr) {
+    return arr.map(function(element) {
+        return element;
+    });
+};
+
+function mapToDouble(arr) {
+    return arr.map(function(element) {
+        return element * 2;
+    });
+};
+
+function mapToSquare(arr) {
+    return arr.map(function(element) {
+        return element * element;
+    });
+};  
+
+function reduceToTotal(arr, startingPoint = 0) {
+    let total = startingPoint
+    for(let num of arr){
+        total = total + num;
+    }
+    return total;
+};
+
+function reduceToAllTrue(arr) {
+    let returnValue;
+    for (const element of arr) {
+        !!element === true ? returnValue = true : returnValue = false;
+    }
+    return returnValue;
+};
+
+function reduceToAnyTrue(arr) {
+    let returnValue = false;
+    for (const element of arr) {
+        if (!!element === true) {
+            returnValue = true;
+        };
+    }
+    return returnValue;
+};
